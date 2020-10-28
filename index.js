8d54a29fe944d46d0efd88b38765b9f0ea2e1c4b
Mikko Paderes <mikkopaderes@outlook.com>
2020-10-29 00:10:12 +0800

    Update ember-firebase-service dependency

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 'use strict';
 
 const Config = require('./lib/config');
-const Funnel = require('broccoli-funnel');
 const MergeTrees = require('broccoli-merge-trees');
-const fastbootTransform = require('fastboot-transform');
 
 module.exports = {
   name: 'ember-cloud-firestore-adapter',
@@ -21,8 +19,8 @@ module.exports = {
 
     this.serviceWorkerOption = Object.assign({}, this.serviceWorkerOption, { firebaseConfig });
 
-    app.import('vendor/fastboot-shims/firebase/firebase-auth.js');
-    app.import('vendor/fastboot-shims/firebase/firebase-firestore.js');
+    app.import('vendor/ember-firebase-service/firebase/firebase-auth.js');
+    app.import('vendor/ember-firebase-service/firebase/firebase-firestore.js');
 
     if (app.env !== 'production') {
       app.import('node_modules/mock-cloud-firestore/dist/mock-cloud-firestore.js');
@@ -34,14 +32,5 @@ module.exports = {
     const configFile = new Config([appTree], this.serviceWorkerOption);
 
     return new MergeTrees([swTree, configFile]);
-  },
-
-  treeForVendor(defaultTree) {
-    const browserVendorLib = new Funnel('node_modules', {
-      destDir: 'fastboot-shims',
-      files: ['firebase/firebase-auth.js', 'firebase/firebase-firestore.js'],
-    });
-
-    return new MergeTrees([defaultTree, fastbootTransform(browserVendorLib)]);
   },
 };
