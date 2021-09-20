53444589d3f4bacc186e66b08d106758bd3cee16
Mikko Paderes <rmmpaderes@gmail.com>
2021-09-20 23:44:04 +0800

    Support FastBoot

--- addon/services/-firebase.ts
+++ addon/services/-firebase.ts
@@ -5,17 +5,33 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
+declare var FastBoot: any;
+
+function setupEnvFirebase() {
+  if (typeof FastBoot === 'undefined') {
+    return firebase;
+  }
+
+  const envFirebase = FastBoot.require('firebase/compat/app');
+
+  FastBoot.require('firebase/compat/auth');
+  FastBoot.require('firebase/compat/firestore');
+
+  return envFirebase;
+}
+
 export default {
   isServiceFactory: true,
 
   create(context: ApplicationInstance): firebase.app.App {
     const config = getOwner(context).resolveRegistration('config:environment');
+    const envFirebase = setupEnvFirebase();
     let firebaseApp;
 
     try {
-      firebaseApp = firebase.app();
+      firebaseApp = envFirebase.app();
     } catch (e) {
-      firebaseApp = firebase.initializeApp(config['ember-cloud-firestore-adapter'].firebaseConfig);
+      firebaseApp = envFirebase.initializeApp(config['ember-cloud-firestore-adapter'].firebaseConfig);
     }
 
     return firebaseApp;
