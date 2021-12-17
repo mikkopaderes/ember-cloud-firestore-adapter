95d1625a5b5c83d69f47cd4bda4efd8ef22d6095
Charles Fries <charliefries@icloud.com>
2021-12-17 11:58:37 -0800

    Fix dummy modular format

--- tests/dummy/app/controllers/query.ts
+++ tests/dummy/app/controllers/query.ts
@@ -1,14 +1,22 @@
 import { action } from '@ember/object';
 import Controller from '@ember/controller';
+import {
+  query,
+  orderBy,
+  limit,
+} from 'ember-cloud-firestore-adapter/firebase/firestore';
 
-import firebase from 'firebase/compat/app';
+import type firebase from 'firebase/compat/app';
 
 export default class QueryController extends Controller {
   @action
   public async handleLoadMoreClick(): Promise<void> {
-    this.model.set('query.filter', (reference: firebase.firestore.CollectionReference) => {
-      reference.orderBy('name').limit(5)
-    });
+    this.model.set(
+      'query.filter',
+      (reference: firebase.firestore.CollectionReference) => {
+        return query(reference, orderBy('name'), limit(5));
+      }
+    );
 
     await this.model.update();
   }
