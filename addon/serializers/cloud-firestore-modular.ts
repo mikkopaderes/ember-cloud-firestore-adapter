8c65224647257a4ae618094bd14049acd5e30f9a
Charles Fries <charliefries@icloud.com>
2024-04-06 16:23:42 -0700

    Ch

--- addon/serializers/cloud-firestore-modular.ts
+++ addon/serializers/cloud-firestore-modular.ts
@@ -62,15 +62,15 @@ interface ModelClass {
     callback: (this: T, key: string, attribute: AttributeSchema) => void,
     binding?: T
   ): void;
-  eachRelationship<T>(
-    callback: (this: T, key: string, relationship: RelationshipSchema) => void,
-    binding?: T
-  ): void;
   eachTransformedAttribute<T>(
     callback: (this: T, key: string, relationship: RelationshipSchema) => void,
     binding?: T
   ): void;
   determineRelationshipType(descriptor: { kind: string, type: string }, store: Store): string;
+  eachRelationship<T>(
+    callback: (this: T, key: string, relationship: RelationshipSchema) => void,
+    binding?: T
+  ): void;
 }
 
 export default class CloudFirestoreSerializer extends JSONSerializer {
