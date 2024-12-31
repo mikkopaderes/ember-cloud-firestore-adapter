5479f65a04d2bb406f99385cf95ddb91537b7249
Charles Fries <charliefries@icloud.com>
2024-12-30 17:23:11 -0800

    Remove dummy app type declaration

--- types/dummy/app.d.ts
+++ types/dummy/app.d.ts
@@ -1,8 +1,2 @@
 import 'ember-source/types';
 import 'ember-source/types/preview';
-
-declare module 'dummy/app' {
-  import Application from '@ember/application';
-
-  export default class App extends Application {}
-}
